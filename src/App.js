95a6b7a86f59fb07404e403b54709a6a2ea64b5e
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2017-06-24 01:34:46 +0900

    first commit.

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import {SystemModel, CanvasVisualizer, PlayButton, ResetButton} from './libs.js'
+
+
+class GoL extends SystemModel {
+    constructor(width, height) {
+        super()
+        this.width = width
+        this.height = height
+    }
+    
+    init() {
+        this.cells = [...Array(this.width)].map(() => Array(this.height));
+        this.cellsNext = [...Array(this.width)].map(() => Array(this.height));
+        for(let x = 0; x < this.cells.length; x++) {
+            for (let y = 0; y < this.cells[x].length; y++) {
+                this.cells[x][y] = Math.floor(Math.random()*2);
+            }
+        }
+    }
+    
+    update() {
+        const width = this.width
+        const height = this.height
+
+        for(let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                const nw = this.cells[(x-1+width)%width][(y-1+height)%height];
+                const n  = this.cells[ x               ][(y-1+height)%height];
+                const ne = this.cells[(x+1)%width      ][(y-1+height)%height];
+                const w  = this.cells[(x-1+width)%width][y]
+                const c  = this.cells[ x               ][y]
+                const e  = this.cells[(x+1)%width      ][y]
+                const sw = this.cells[(x-1+width)%width][(y+1)%height];
+                const s  = this.cells[ x               ][(y+1)%height];
+                const se = this.cells[(x+1)%width      ][(y+1)%height];
+                const nsum = nw + n + ne + w + e + sw + s + se;
+                let new_state = 0;
+                if ( nsum===3 || (nsum===2 && c===1)) {
+                    new_state = 1;
+                }
+                this.cellsNext[x][y] = new_state;
+            }
+        }
+        const tmp = this.cellsNext;
+        this.cellsNext = this.cells;
+        this.cells = tmp;
+    }
+}
+
+let draw_gol = function(canvas, gol) {
+    const ctx = canvas.getContext('2d');
+    const width = canvas.width;
+    const height = canvas.height;
+    let imgData = ctx.createImageData(width, height);
+    for(let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const s = gol.cells[x][y]
+            const i = x + y*width;
+            imgData.data[i*4+0] = (1-s)*255;
+            imgData.data[i*4+1] = (1-s)*255;
+            imgData.data[i*4+2] = (1-s)*255;
+            imgData.data[i*4+3] = 255;
+        }
+    }
+    ctx.putImageData(imgData, 0, 0);
+}
+
+let gol = new GoL(600, 400)
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <CanvasVisualizer width={gol.width} height={gol.height} system={gol} draw_func={draw_gol} />
+                <div>
+                    <PlayButton system={gol} />
+                    <ResetButton system={gol} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/libs.js
@@ -0,0 +1,113 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+
+import FlatButton from 'material-ui/FlatButton';
+import PlayArrow from 'material-ui/svg-icons/av/play-arrow';
+import Pause from 'material-ui/svg-icons/av/pause';
+import Replay from 'material-ui/svg-icons/av/replay';
+
+
+export class SystemModel {
+    constructor() {
+        this.updateListeners = []
+    }
+    
+    run() {
+        this.running = true
+        const runner = ()=>{
+            this.update()
+            this.callUpdateListener()
+            if (this.running) {
+                requestAnimationFrame(runner.bind(this))
+            }
+        }
+        runner()
+    }
+    
+    stop() {
+        this.running = false
+    }
+    
+    callUpdateListener() {
+        for (let f of this.updateListeners) {
+            f(this)
+        }
+    }
+    
+    addUpdateListener(listener) {
+        this.updateListeners.push(listener)
+    }
+}
+
+let canvas_style = {
+    //width: '100%'
+}
+
+export class CanvasVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.props.system.init()
+    }
+    
+    componentDidMount() {
+        const {canvas} = this.refs;
+        this.props.system.addUpdateListener(this.props.draw_func.bind(null, canvas))
+        this.props.system.callUpdateListener()
+    }
+
+    render() {
+        return (
+            <canvas ref="canvas"
+                width={this.props.width} height={this.props.height}
+                style={canvas_style}
+                />
+        )
+    }
+}
+
+CanvasVisualizer.propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+};
+
+export class PlayButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_pause: this.props.system.running
+        }
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        if (this.props.system.running) {
+            this.props.system.stop()
+        } else {
+            this.props.system.run()
+        }
+        this.setState({
+            is_pause: this.props.system.running
+        })
+    }
+    render() {
+        return <FlatButton icon={this.state.is_pause ? <Pause /> : <PlayArrow />} onClick={this.handleClick} />
+    }
+}
+
+export class ResetButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_pause: this.props.system.running
+        }
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.system.init()
+        this.props.system.callUpdateListener()
+    }
+    render() {
+        return <FlatButton icon={<Replay />} onClick={this.handleClick} />
+    }
+}
